0477590f534d27763d17058a9a40325648c1c870
Burkay <burkayatar5@gmail.com>
2022-08-20 04:57:07 +0300

    commit project

--- assignment3/ViewController.swift
+++ assignment3/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "VW Polo"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "BackgroundColor")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "TextColor")!, .font: UIFont(name: "Montserrat", size: 22)!]
+        
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Add Favorite", image: UIImage(systemName: "heart"), handler: { (_) in
+                }),
+                UIAction(title: "Share", image: UIImage(systemName: "square.and.arrow.down"), handler: { (_) in
+                }),
+                UIAction(title: "Reserve a car", image: UIImage(systemName: "car"), handler: { (_) in
+                }),
+                UIAction(title: "Report Problem", image: UIImage(systemName: "exclamationmark.triangle"), attributes: .destructive, handler: { (_) in
+                })
+            ]
+        }
+        
+        var barButtonMenu: UIMenu {
+            return UIMenu(title: "Options",
+                          image: UIImage(systemName: "questionmark.circle")?.withTintColor(UIColor(named: "TextColor")!),
+                          identifier: nil, options: [], children: menuItems)
+        }
+        //-TODO: withTintColor method not working
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "", image: UIImage(systemName: "line.3.horizontal", withConfiguration: UIImage.SymbolConfiguration(scale: .large))!.withTintColor(UIColor(named: "TextColor")!), primaryAction: nil, menu: barButtonMenu)
+        
     }
-
-
 }
 
